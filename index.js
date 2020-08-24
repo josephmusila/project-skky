1b27d86e581344f105d5e436a7cfb25da8339a52
joseph musila <josemusila03@gmail.com>
2020-08-24 23:25:30 +0300

    fixed the card respons to change of window width,added the presenter profile cards

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
     $(".prog").accordion();
-    $("#projects").tabs();
-    $("#reach").animate
+    $("#projects").tabs({
+        event:"mouseover",
+        collapsible:true
+    });
+   $("#standings").click(function(){
+       $("#standings").effect({
+           effect:"explode",
+           easing:"easeInExpo",
+           pieces:100,
+           duration:10000
+       });
+   });
+  
 });
 //testing doc overall hover
 document.querySelectorAll('div')[0].onmouseover=function(e){
@@ -12,31 +23,17 @@ document.querySelectorAll('div')[0].onmouseover=function(e){
 function vlaidate(){
     var country=document.getElementById("country").value;
     var email=document.getElementById("email").value;
+    var atposition=email.indexOf("@");
+    var dotposition=email.lastIndexOf(".");
+    if(atposition<1|| dotposition +2 || dotposition+2>=email.length);
+    {
+        alert("please enter a valid email");
+        return false;
+    }
 }
 $(function(){
-    $("#signin").click(function(){
-        $("#signin-dialogue").dialog({width:500,height:220,modal:true,buttons:{
-           " log in":function(){
-               var username=document.getElementById("username").value;
-               var password=document.getElementById("password").value;
-               if(username=="" || password==" "){
-                   document.getElementById("err").innerHTML="please fill the fields";
-               }else{
-                $("#sigin-dialogue").val("");
-                $(this).dialog("close");
-               }
-               
-           },
-            "close":function(){
-                $("#sigin-dialogue").val("");
-                $(this).dialog("close");
-            }
-        }});
-    });
-});
-$(function(){
     $("#registerbtn").click(function(){
-        $("#register").dialog({width:500,modal:true,
+        $("#register").dialog({width:400,modal:true,
         buttons:{
             "SUBMIT":function(){
                 var username1=document.getElementById("username1").value;
@@ -56,3 +53,50 @@ $(function(){
         }});
     });
 });
+$(function(){
+    $("#signin").click(function(){
+        $("#signin-dialogue").dialog({width:400,height:220,modal:true,buttons:{
+           " log in":function(){
+               var username=document.getElementById("username").value;
+               var password=document.getElementById("password").value;
+               if(username=="" || password==" "){
+                   document.getElementById("err").innerHTML="please fill the fields";
+               }else{
+                $("#sigin-dialogue").val("");
+                $(this).dialog("close");
+               }
+               
+           },
+            "close":function(){
+                $("#sigin-dialogue").val("");
+                $(this).dialog("close");
+            }
+        }});
+    });
+});
+
+$(function(){
+    $("#username1").tooltip({
+        content:"enter your name"
+       
+        
+    });
+  //resetting accordion header styles
+  $("#rog").click(function(){
+      if(this.id=="rog"){
+          $("#rog").addClass("new","slow");
+      }else{
+          $("#rog").removeClass("new","fast");
+      }
+  })
+
+});
+function tip(){
+    $("#android").tooltip({
+        content:"the app ui on android phones"
+    });
+    $("#iphone").tooltip({
+        content:"the app ui on iphones phones"
+    });
+}document.getElementById("android").addEventListener("mouseover",tip);
+func
